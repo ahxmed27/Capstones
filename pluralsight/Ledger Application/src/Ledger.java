ac47b79a338ca14265221d96d20f975da9d06d16
Ahmed Mohamed <ahmed272@uw.edu>
2025-10-17 09:45:35 -0700

    Removed useless code and cleaned up code. Also made sure transactions are shown by most recent.

--- pluralsight/Ledger Application/src/Ledger.java
+++ pluralsight/Ledger Application/src/Ledger.java
@@ -6,7 +6,7 @@
 public class Ledger {
 
 
-    public static void display(){
+    public static void display() {
         System.out.println("A) All - Display all entries");
         System.out.println("D) Deposits");
         System.out.println("P) Payments");
@@ -14,65 +14,60 @@ public class Ledger {
         System.out.println("H) Home - Go back to the Home page");
         System.out.print("Answer: ");
     }
-    public static void entries(){
-        for(Transaction x: Debit.list){
-            System.out.println(x.toString());
+
+    public static void entries() {
+        for (int i = Debit.list.size() - 1; i >= 0; i--) {
+            System.out.println(Debit.list.get(i).toString());
         }
     }
 
-    public static void deposits(){
+    public static void deposits() {
         boolean deposit = false;
-        int count = 0;
-        for(Transaction x: Debit.list){
-            if(x.getAmount() > 0){
-                System.out.println(x.toString());
+        for (int i = Debit.list.size() - 1; i >= 0; i--) {
+            if (Debit.list.get(i).getAmount() > 0) {
+                System.out.println(Debit.list.get(i).toString());
                 deposit = true;
-                count++;
             }
-            else if(!deposit && count == 0 ){
-                System.out.println("No deposit records found.");
-                count++;
-            }
+        }
+        if (!deposit) {
+            System.out.println("No deposit records found.");
         }
     }
 
-    public static void payments(){
+    public static void payments() {
         boolean payment = false;
-        int count = 0;
-        for(Transaction x: Debit.list){
-            if(x.getAmount() < 0){
-                System.out.println(x.toString());
+        for (int i = Debit.list.size() - 1; i >= 0; i--) {
+            if (Debit.list.get(i).getAmount() < 0) {
+                System.out.println(Debit.list.get(i).toString());
                 payment = true;
-                count++;
-            }
-            else if(!payment && count == 0)
-                System.out.println("No Payment records found.");
-            count++;
             }
         }
+        if (!payment)
+            System.out.println("No Payment records found.");
+    }
 
-        public static void reportsDisplay(){
-            System.out.println("1) Month To Date transactions");
-            System.out.println("2) Previous Month transactions");
-            System.out.println("3) Year To Date transactions");
-            System.out.println("4) Previous Year transactions");
-            System.out.println("5) Search by Vendor");
-            System.out.println("0) Back - Go back to the Ledger page");
-            System.out.print("Answer: ");
+    public static void reportsDisplay() {
+        System.out.println("1) Month To Date transactions");
+        System.out.println("2) Previous Month transactions");
+        System.out.println("3) Year To Date transactions");
+        System.out.println("4) Previous Year transactions");
+        System.out.println("5) Search by Vendor");
+        System.out.println("0) Back - Go back to the Ledger page");
+        System.out.print("Answer: ");
 
-        }
+    }
 
     public static void monthDate() {
         String[] split;
         boolean check = false;
-        for(Transaction x: Debit.list){
+        for (Transaction x : Debit.list) {
             split = x.getDate().split("-");
-            if(Integer.parseInt(split[0]) == 2025 && Integer.parseInt(split[1]) == 10){
+            if (Integer.parseInt(split[0]) == 2025 && Integer.parseInt(split[1]) == 10) {
                 System.out.println(x.toString());
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             System.out.println("No transaction records found.");
             check = true;
         }
@@ -82,14 +77,14 @@ public class Ledger {
     public static void previousMonth() {
         String[] split;
         boolean check = false;
-        for(Transaction x: Debit.list){
+        for (Transaction x : Debit.list) {
             split = x.getDate().split("-");
-            if(Integer.parseInt(split[0]) == 2025 && Integer.parseInt(split[1]) == 9) {
+            if (Integer.parseInt(split[0]) == 2025 && Integer.parseInt(split[1]) == 9) {
                 System.out.println(x.toString());
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             System.out.println("No transaction records found.");
             check = true;
         }
@@ -98,14 +93,14 @@ public class Ledger {
     public static void YearDate() {
         String[] split;
         boolean check = false;
-        for(Transaction x: Debit.list){
+        for (Transaction x : Debit.list) {
             split = x.getDate().split("-");
-            if(Integer.parseInt(split[0]) == 2025){
+            if (Integer.parseInt(split[0]) == 2025) {
                 System.out.println(x.toString());
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             System.out.println("No transaction records found.");
             check = true;
         }
@@ -121,7 +116,7 @@ public class Ledger {
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             System.out.println("No transaction records found.");
             check = true;
         }
@@ -129,13 +124,13 @@ public class Ledger {
 
     public static void searchByVendor(String next) {
         boolean check = false;
-        for(Transaction x: Debit.list){
-            if(x.getVendor().equalsIgnoreCase(next)) {
+        for (Transaction x : Debit.list) {
+            if (x.getVendor().equalsIgnoreCase(next)) {
                 System.out.println(x.toString());
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             System.out.println("No vendor match found.");
             check = true;
         }
